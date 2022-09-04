a3543911f9ec3739b5817a5b56f47e4bc803bbca
kalailincoln <112810051+kalailincoln@users.noreply.github.com>
2022-09-04 18:14:39 +0530

    Add files via upload

---
+++ Loops/src/loops/employeedata.java
@@ -0,0 +1,29 @@
+package loops;
+
+import java.util.Scanner;
+
+public class employeedata {
+	public static void main(String[] args) {
+	int i = 1;
+	int n = 5;
+	
+	
+	Scanner obj = new Scanner(System.in);
+	
+		
+	
+	System.out.println("How many days of leave eligible");
+	n = obj.nextInt();
+	
+	
+	while(i<=n); {                             // while loop
+		System.out.println("Approved");
+		i++;
+		
+	}
+	}
+	
+}
+  
+
+
